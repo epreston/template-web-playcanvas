d30e78483bb844a9f31cc101a09f08c7200e8e0e
Ed Preston <epreston@prestonsoft.com>
2023-08-27 17:42:23 +1000

    Update main.js
    reorder statements for clarity

--- src/main.js
+++ src/main.js
@@ -28,14 +28,14 @@ const camera = new pc.Entity('camera');
 camera.addComponent('camera', {
     clearColor: new pc.Color(0.1, 0.1, 0.1)
 });
-app.root.addChild(camera);
 camera.setPosition(0, 0, 3);
+app.root.addChild(camera);
 
 // create directional light entity
 const light = new pc.Entity('light');
 light.addComponent('light');
-app.root.addChild(light);
 light.setEulerAngles(45, 0, 0);
+app.root.addChild(light);
 
 // rotate the box according to the delta time since the last frame
 app.on('update', dt => box.rotate(10 * dt, 20 * dt, 30 * dt));
