26cc0a71ac82a5626617064c67aa01a10fef34d9
Ed Preston <epreston@prestonsoft.com>
2026-01-15 07:13:05 +1100

    Modify indent rule
    bugfix for playcanvas rule to be compatible with prettier, allow promises (.then and .catch) to be indented

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,10 @@ export default [
     },
     {
         name: 'project/config/settings',
+        rules: {
+            // bugfix : allow promises (.then and .catch) to be indented
+            indent: ['error', 4, { MemberExpression: 'off' }]
+        },
         languageOptions: {
             ecmaVersion: 2024,
             sourceType: 'module',
@@ -24,21 +28,21 @@ export default [
         name: 'project/config/playcanvas',
         languageOptions: {
             globals: {
-                'Ammo': 'readonly',
-                'earcut': 'readonly',
-                'glslang': 'readonly',
-                'GPUBufferUsage': 'readonly',
-                'GPUColorWrite': 'readonly',
-                'GPUMapMode': 'readonly',
-                'GPUShaderStage': 'readonly',
-                'GPUTextureUsage': 'readonly',
-                'opentype': 'readonly',
-                'pc': 'readonly',
-                'TWEEN': 'readonly',
-                'twgsl': 'readonly',
-                'webkitAudioContext': 'readonly',
-                'XRRay': 'readonly',
-                'XRWebGLLayer': 'readonly'
+                Ammo: 'readonly',
+                earcut: 'readonly',
+                glslang: 'readonly',
+                GPUBufferUsage: 'readonly',
+                GPUColorWrite: 'readonly',
+                GPUMapMode: 'readonly',
+                GPUShaderStage: 'readonly',
+                GPUTextureUsage: 'readonly',
+                opentype: 'readonly',
+                pc: 'readonly',
+                TWEEN: 'readonly',
+                twgsl: 'readonly',
+                webkitAudioContext: 'readonly',
+                XRRay: 'readonly',
+                XRWebGLLayer: 'readonly'
             }
         }
     }
